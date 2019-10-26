6180b07b04316b2ba682695aab5f533c579b8e31
Sven Ohl <svenohl@googlemail.com>
2019-10-26 23:08:30 +0200

    Timing and messages
    Report elappsed time and write messages to window

--- prepare/xmsg2tab.py
+++ prepare/xmsg2tab.py
@@ -4,8 +4,13 @@
 # Start with a message
 print("Start parsing ...")
 
+
 # import
 import os
+import time
+
+# timing
+timing_onset = time.time()
 
 # set path
 path_wd = os.getcwd() #get working directory
@@ -17,13 +22,16 @@ path_tab = path_parent+"/tab/"
 path_msg_files = os.listdir(path_msg)
 for filename_in in path_msg_files:
     #filename_in  = "rIM1ap02.msg"
-    filename_out = filename_in[0:8]
+    filename_short=filename_in[0:8]
+    filename_out = filename_short
     filename_out +=  ".txt"
     filename_out = os.path.join(path_tab,filename_out)
     filename_in = os.path.join(path_msg,filename_in)
 
     # open a file
     f_tmp = open(filename_in,'r')
+    print(filename_short)
+    numlines = 0
 
     # open file for output
     fout = open(filename_out,'w')
@@ -73,15 +81,26 @@ for filename_in in path_msg_files:
                 outline   = map(str,outline)
                 outline   = '\t'.join(outline)
                 outline  += "\n"
-            
+                newtrial = True
+
                 # check if outline of a trial is complete and only then save
                 if (bool(trial) == bool(EVENT_FixationDot) == bool(EVENT_MemoryOn)
                 == bool(EVENT_MemoryOff) == bool(EVENT_GoSignal) 
                 == bool(EVENT_ProbeOn)== bool(EVENT_ClearScreen) 
                 == bool(TRIAL_ENDE)== bool(trialdata)==True):
                     fout.write(outline)
-                    newtrial = True
+                    numlines += 1
+                    
+    # print numlines
+    outmsg = "\t" + "... " + str(numlines)
+    print(outmsg)
 
 # close files
 f_tmp.close()
 fout.close()
+
+#end of timing
+timing_offset = time.time()
+elappsed = timing_offset-timing_onset
+elappsed = round(elappsed,3)
+print("Time elappsed: "+str(elappsed))
